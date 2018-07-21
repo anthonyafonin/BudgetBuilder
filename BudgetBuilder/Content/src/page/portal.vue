e553ef9ad2ead4c406a64558ead2ded434cd93d8
Anthony Afonin <anthonyafonin@gmail.com>
2018-07-21 16:09:05 -0700

    include portal view

---
+++ BudgetBuilder/Content/src/page/portal.vue
@@ -0,0 +1,42 @@
+﻿<template>
+    <div class="portal">
+        Hello portal
+    </div>
+</template>
+
+<script>
+    import api from '@/services/api';
+
+    module.exports = {
+        components: {
+
+        },
+        data: function () {
+            return {
+
+            }
+        },
+        computed: {
+
+        },
+        methods: {
+            login: function () { 
+                var vm = this;
+                var user = {}
+
+                api().post('Account/Login', user)
+                    .then(function (r) {
+                        if (r.Success) {
+                            vm.$store.dispatch('login', r.User);
+                        } 
+                    })
+            }
+        },
+        beforeMount: function(){
+            //this.login();
+        },
+        beforeRouteLeave: function (to, from, next) {
+            next();
+        }
+    }
+</script>
